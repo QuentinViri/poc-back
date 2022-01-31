8bf4d92fb0088578ebd8000191770fd5565e2e7f
Viricel <quentin.viricel98@gmail.com>
2022-01-31 17:02:16 +0100

    commit initial Val

---
+++ src/main/java/com/example/cra_back/controller/BasicAuthController.java
@@ -0,0 +1,19 @@
+package com.example.cra_back.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.cra_back.bean.AuthenticationBean;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping("/api/v1")
+public class BasicAuthController {
+
+    @GetMapping(path = "/basicauth")
+    public AuthenticationBean basicauth() {
+        return new AuthenticationBean("You are authenticated");
+    }
+}
